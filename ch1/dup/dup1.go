b6e5928bedda598ec593f47ec2b89af661080d1f
Javier López Sánchez <javierlopez.jls@gmail.com>
2019-05-01 20:48:23 +0200

    dup v2

--- ch1/dup/dup1.go
+++ ch1/dup/dup1.go
@@ -9,15 +9,33 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup:  %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
 	}
 
-	//NOTE: ignoring potential errors from input.Err()
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	//NOTE: ignoring potential errors from input.Err()
 }
